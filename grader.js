5a35adc5d3df167e6c4ebf0f7bcb1943c99edb25
minenhle <minenhle08@gmail.com>
2013-07-07 15:10:51 +0000

    Grader

--- grader.js
+++ grader.js
@@ -29,7 +29,7 @@ var CHECKSFILE_DEFAULT = "checks.json";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-	console.log("%s does not exist. Existing.", instr);
+	console.log("%s does not exist. Exiting.", instr);
 	process.exit(1); //http://nodejs.org/api/process.html#process_process_exit_code
     }
     return inst;
@@ -61,7 +61,7 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
-    program.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT).option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT).parse(process.argv);
+    program.option('-c, --checks <check_file>', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT).option('-f, --file <html_file>', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT).parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson,null,4);
     console.log(outJson);
